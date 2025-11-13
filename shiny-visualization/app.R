1b902cffbf40c84b3f0785225ffc46cb48d07d5b
Badri Jagan <bjagan@mta.ca>
2025-11-13 12:19:10 -0400

    #44 The Collaborations map and Individual search doesn't work as expected
    The error indicated that the code was trying to access a column named title, which does not exist in the uploaded CSV file. Because of this, the app failed to render outputs in sections where the "title" column was used. So once the "title" column was commented out the map and other aspects of the app started to work.

--- shiny-visualization/app.R
+++ shiny-visualization/app.R
@@ -219,7 +219,8 @@ server <- function(input, output, session) {
     
     # Get unique locations with their associated organizations
     unique_institutions <- data() %>%
-      select(org2, city2, region2, country2, lat2, lng2, title) %>%
+      # select(org2, city2, region2, country2, lat2, lng2, title) %>%
+      select(org2, city2, region2, country2, lat2, lng2) %>%
       filter(!is.na(lat2) & !is.na(lng2)) %>%
       group_by(lat2, lng2) %>%
       summarise(
@@ -237,7 +238,7 @@ server <- function(input, output, session) {
       summarise(
         `org2 occurrence` = n(),
         dois = list(unique(doi)),
-        titles = list(unique(title)),
+        # titles = list(unique(title)),
         .groups = 'drop'
       )
     
@@ -370,11 +371,14 @@ server <- function(input, output, session) {
         data() %>%
           filter(!is.na(org2) & org2 != "" & !str_detect(org2, "^\\s*$")) %>%
           filter(grepl(search_term, orcid1, ignore.case = TRUE)) %>%
-          select(doi, title) %>%
+          # select(doi, title) %>%
+          # distinct() %>%
+          # filter(!is.na(doi)) & (!is.na(title)) %>%
+          # rename(DOI = doi, Title = title)
+          select(doi) %>%
           distinct() %>%
-          filter(!is.na(doi) & !is.na(title)) %>%
-          rename(DOI = doi, 
-                 Title = title)
+          filter(!is.na(doi)) %>%
+          rename(DOI = doi)
       })
 
 
